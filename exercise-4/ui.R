155873e3fa780f6b2827c155395a01bad34552f8
Jonathan Thiem <jsttsj@hotmail.com>
2016-11-15 15:13:35 -0800

    fixed

--- exercise-4/ui.R
+++ exercise-4/ui.R
@@ -36,9 +36,9 @@ shinyUI(navbarPage('Electoral College',
                                 sidebarPanel(
                                 # Make a textInput widget for searching for a state in your scatter plot
                    
-                              textInput('scattervar',label = 'Variable to Scatter',value = "start..."),
+                              textInput('scattervar',label = 'Variable to Scatter',value = "start...")),
                               # Create a main panel, in which you should display your plotly Scatter plot
                               mainPanel(
                                 plotlyOutput('scatter')
                               )))
-)))
+))
